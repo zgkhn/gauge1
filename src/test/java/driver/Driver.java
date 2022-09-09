bc169bed15c8c589f5a2775063d865f3b28a8fa9
gokhan <gkhnznkldk@gmail.com>
2022-09-10 01:51:13 +0300

    sss

--- src/test/java/driver/Driver.java
+++ src/test/java/driver/Driver.java
@@ -1,22 +1,30 @@
 package driver;
-
-import com.thoughtworks.gauge.AfterSuite;
-import com.thoughtworks.gauge.BeforeSuite;
+import com.thoughtworks.gauge.AfterScenario;
+import com.thoughtworks.gauge.BeforeScenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
     // Holds the WebDriver instance
     public static WebDriver webDriver;
-
-    // Initialize a webDriver instance of required browser
-    // Since this does not have a significance in the application's business domain, the BeforeSuite hook is used to instantiate the webDriver
-    @BeforeSuite
-    public void initializeDriver(){
-        webDriver = DriverFactory.getDriver();
+    public static Boolean platformdocker = true;
+    @BeforeScenario
+    public void initializeDriver() throws MalformedURLException {
+        if (platformdocker) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            webDriver = new RemoteWebDriver(new URL("http://172.18.0.3:4444/wd/hub"), options);
+        } else  {
+            webDriver = DriverFactory.getDriver();
+        }
     }
-
     // Close the webDriver instance
-
 
 }
